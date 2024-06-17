b1c9b05b8cd14a3c041fd449b221e1cb4fe45c4e
elruin <arnadoairiellemarie@gmail.com>
2024-06-18 03:27:27 +0800

    made changes

--- app/Core/Config.php
+++ app/Core/Config.php
@@ -9,17 +9,12 @@
 function defineRoot() {
     if($_SERVER['SERVER_NAME'] == 'localhost') {
         // database configuration
-        // $config = simplexml_load_file('../App.config');
-        // define('DBNAME', $config->database->dbname);
-        // define('DBHOST', $config->database->host);
-        // define('DBUSER', $config->database->username);
-        // define('DBKEY', $config->database->password);
-        // define('PORT', $config->database->port);
-        define('DBNAME', 'postgres');
-        define('DBHOST', 'aws-0-ap-southeast-1.pooler.supabase.com');
-        define('DBUSER', 'postgres.rvtypwqfctwjnljbuixi');
-        define('DBKEY', 'lDZUDVrwXswnHFWKhDR9DLl1');
-        define('PORT', '6543');
+        $config = simplexml_load_file('../App.config');
+        define('DBNAME', $config->database->dbname);
+        define('DBHOST', $config->database->host);
+        define('DBUSER', $config->database->username);
+        define('DBKEY', $config->database->password);
+        define('PORT', $config->database->port); 
         define('ROOT', 'http://localhost/Time-Tracker/public/');
         define('APP', 'http://localhost/Time-Tracker/app/');
     } else {
