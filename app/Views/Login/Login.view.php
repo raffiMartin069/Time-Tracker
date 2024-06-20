d9a59f34117762b15346ddc6c54ff309100be817
rafael.d.mrtinez@gmail.com <raffimartin069@gmail.com>
2024-06-20 21:30:22 +0800

    Logo added

--- app/Views/Login/Login.view.php
+++ app/Views/Login/Login.view.php
@@ -6,6 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>Login | WTN</title>
+  <link rel="icon" type="image/x-icon" href="<?php ROOT ?>assets/img/login/logo_wtn.png">
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
