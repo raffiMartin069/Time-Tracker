4f1c16ac31dbbc49ea521dafdba70cd13ca3ef72
elruin <arnadoairiellemarie@gmail.com>
2024-08-04 01:19:59 +0800

    Modified labels

--- app/Models/AdminModels/AllManageAdminModel.php
+++ app/Models/AdminModels/AllManageAdminModel.php
@@ -2,117 +2,44 @@
 final class AllManageAdminModel
 
 { 
-    private $EMP_ID;
-    private $FNAME;
-    private $MNAME;  
-    private $LNAME; 
-    private $BIRTH_DATE;
-    private $HIRED_DATE;
-
+    private $ID; 
+    private $EMPLOYEE;  
 
     public function __construct($data) { 
-        $this->EMP_ID = $data['EMP_ID'];
-        $this->FNAME = $data['FNAME'];
-        $this->MNAME = $data['MNAME']; 
-        $this->LNAME = $data['LNAME'];
-        $this->BIRTH_DATE = $data['BIRTH_DATE'];
-        $this->HIRED_DATE = $data['HIRED_DATE']; 
- 
-     }
+        $this->ID = $data['ID']; 
+        $this->EMPLOYEE = $data['EMPLOYEE'];  
+      }
   
     /**
      * @return mixed
      */
     public function getEMPID()
     {
-        return $this->EMP_ID;
+        return $this->ID;
     }
 
     /**
-     * @param mixed $EMP_ID
+     * @param mixed $ID
      */
-    public function setEMPID($EMP_ID)
+    public function setEMPID($ID)
     {
-        $this->EMP_ID = $EMP_ID;
-    }
- 
-    /**
-     * @return mixed
-     */
-    public function getFNAME()
-    {
-        return $this->FNAME;
-    }
-
-    /**
-     * @param mixed $FNAME
-     */
-    public function setFNAME($FNAME)
-    {
-        $this->FNAME = $FNAME;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getMNAME()
-    {
-        return $this->MNAME;
-    }
-
-    /**
-     * @param mixed $MNAME
-     */
-    public function setMNAME($MNAME)
-    {
-        $this->MNAME = $MNAME;
+        $this->ID = $ID;
     } 
 
     /**
      * @return mixed
      */
-    public function getLNAME()
+    public function getEMPNAME()
     {
-        return $this->LNAME;
+        return $this->EMPLOYEE;
     }
 
     /**
-     * @param mixed $LNAME
+     * @param mixed $EMPLOYEE
      */
-    public function setLNAME($LNAME)
+    public function setEMPNAME($EMPLOYEE)
     {
-        $this->LNAME = $LNAME;
-    }
+        $this->EMPLOYEE = $EMPLOYEE;
+    }  
 
-    /**
-     * @return mixed
-     */
-    public function getBIRTHDATE()
-    {
-        return $this->BIRTH_DATE;
-    }
-
-    /**
-     * @param mixed $BIRTH_DATE
-     */
-    public function setBIRTHDATE($BIRTH_DATE)
-    {
-        $this->BIRTH_DATE = $BIRTH_DATE;
-    } 
-
-    /**
-     * @return mixed
-     */
-    public function getHIREDDATE()
-    {
-        return $this->HIRED_DATE;
-    }
-
-    /**
-     * @param mixed $HIRED_DATE
-     */
-    public function setHIREDDATE($HIRED_DATE)
-    {
-        $this->HIRED_DATE = $HIRED_DATE;
-    } 
-}
+ }
