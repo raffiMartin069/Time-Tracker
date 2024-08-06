8b746f2856ce874d8419f6aede0a2b1067515911
airielle <122017843+elruin@users.noreply.github.com>
2024-08-07 03:53:18 +0800

    Updated code to ensure break periods remained close after filter/reset

--- public/scripts/employeeDailyReport.js
+++ public/scripts/employeeDailyReport.js
@@ -1,16 +1,16 @@
 $(".view-breaks-btn").click(function () {
   var $button = $(this);
-  var dailyId = $button.closest("tr").find(".daily-id").text();
   var $breakDetailsRow = $button.closest("tr").next(".break-details-row");
   var $accordionBody = $breakDetailsRow.find(".accordion-body");
 
   $breakDetailsRow.toggle();
-
-  if ($breakDetailsRow.is(":visible")) {
-    $button.attr("src", ROOT + "assets/img/breaks-up-arrow.png");
-  } else {
-    $button.attr("src", ROOT + "assets/img/breaks-down-arrow.png");
-  }
+  
+  $button.attr(
+    "src",
+    $breakDetailsRow.is(":visible")
+      ? ROOT + "assets/img/breaks-up-arrow.png"
+      : ROOT + "assets/img/breaks-down-arrow.png"
+  );
 
   if (
     $breakDetailsRow.is(":visible") &&
@@ -20,12 +20,11 @@ $(".view-breaks-btn").click(function () {
       url: "Admin/BreakStamps",
       method: "GET",
       data: {
-        daily_id: dailyId,
+        daily_id: $button.closest("tr").find(".daily-id").text(),
       },
       dataType: "json",
       success: function (data) {
         var content = '<table class="table table-responsive"><thead><tr>';
-
         for (let i = 0; i < data.length; i++) {
           content +=
             "<th>Break " +
@@ -34,7 +33,6 @@ $(".view-breaks-btn").click(function () {
             (i + 1) +
             " End</th>";
         }
-
         content += "</tr></thead><tbody>";
         if (data.length > 0) {
           content += "<tr>";
@@ -57,7 +55,6 @@ $(".view-breaks-btn").click(function () {
         $accordionBody.html(content);
       },
       error: function (xhr, status, error) {
-        console.error("AJAX error:", status, error);
         $accordionBody.html("Error fetching break data");
       },
     });
