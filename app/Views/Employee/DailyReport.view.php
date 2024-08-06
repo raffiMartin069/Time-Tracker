3542928c8834336334ac8f98d3b6527f12d9e5a1
airielle <122017843+elruin@users.noreply.github.com>
2024-08-07 03:55:56 +0800

    Changed filename for js

--- app/Views/Employee/DailyReport.view.php
+++ app/Views/Employee/DailyReport.view.php
@@ -176,7 +176,7 @@
     <script src="<?= ROOT ?>node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-    <script src="<?= ROOT ?>scripts/datePicker.js"></script>
+    <script src="<?= ROOT ?>scripts/dailyReportsDatePicker.js"></script>
     <script>
         var ROOT = '<?= ROOT ?>';
     </script> 
