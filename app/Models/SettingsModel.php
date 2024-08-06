7e81fde0be6ae04e613f06c0ec3468c9034b7031
airielle <122017843+elruin@users.noreply.github.com>
2024-08-07 02:16:12 +0800

    Modified names

--- app/Models/SettingsModel.php
+++ app/Models/SettingsModel.php
@@ -21,130 +21,82 @@ final class SettingsModel
         $this->ECN = $data['ECN']; 
         $this->IMAGE = $data['IMAGE'];
      }
- 
-      /**
-     * @return mixed
-     */
+  
     public function getPROFILEPIC()
     {
         return $this->IMAGE;
     }
-
-    /**
-     * @param mixed $IMAGE
-     */
+ 
     public function setPROFILEPIC($IMAGE)
     {
         $this->IMAGE = $IMAGE;
     }
-
-    /**
-     * @return mixed
-     */
+ 
     public function getEMPID()
     {
         return $this->EMP_ID;
-    }
-
-    /**
-     * @param mixed $EMP_ID
-     */
+    } 
+    
     public function setEMPID($EMP_ID)
     {
         $this->EMP_ID = $EMP_ID;
-    }
- 
-    /**
-     * @return mixed
-     */
+    } 
+    
     public function getFNAME()
     {
         return $this->FNAME;
     }
-
-    /**
-     * @param mixed $FNAME
-     */
+ 
     public function setFNAME($FNAME)
     {
         $this->FNAME = $FNAME;
     }
-
-    /**
-     * @return mixed
-     */
+ 
     public function getMNAME()
     {
         return $this->MNAME;
     }
-
-    /**
-     * @param mixed $MNAME
-     */
+ 
     public function setMNAME($MNAME)
     {
         $this->MNAME = $MNAME;
     } 
-
-    /**
-     * @return mixed
-     */
+ 
     public function getLNAME()
     {
         return $this->LNAME;
     }
-
-    /**
-     * @param mixed $LNAME
-     */
+ 
     public function setLNAME($LNAME)
     {
         $this->LNAME = $LNAME;
     }
-
-    /**
-     * @return mixed
-     */
+ 
     public function getBIRTHDATE()
     {
         return $this->BIRTH_DATE;
     }
-
-    /**
-     * @param mixed $BIRTH_DATE
-     */
+ 
     public function setBIRTHDATE($BIRTH_DATE)
     {
         $this->BIRTH_DATE = $BIRTH_DATE;
     }
-
-     /**
-     * @return mixed
-     */
+ 
     public function getEMAIL()
     {
         return $this->EMAIL;
     }
-
-    /**
-     * @param mixed $EMAIL
-     */
+ 
     public function setEMAIL($EMAIL)
     {
         $this->EMAIL = $EMAIL;
     }
 
-     /**
-     * @return mixed
-     */
     public function getECN()
     {
         return $this->ECN;
     }
-
-    /**
-     * @param mixed $ECN
-     */
+ 
     public function setECN($ECN)
     {
         $this->ECN = $ECN;
