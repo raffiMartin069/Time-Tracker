83f9920261e587bf2665711eca6f9cb3d919f4f7
airielle <122017843+elruin@users.noreply.github.com>
2024-08-07 02:14:11 +0800

    Modified names

--- app/Models/WeeklyReportModel.php
+++ app/Models/WeeklyReportModel.php
@@ -14,82 +14,52 @@ final class WeeklyReportModel
         $this->EMP_ID = $data['EMP_ID'];   
         $this->EMPLOYEE_NAME = $data['EMPLOYEE_NAME'];  
     }  
-
-    /**
-     * @return mixed
-     */
+ 
     public function getWKLYID()
     {
         return $this->WKLY_ID;
     }
- 
-    /**
-     * @param mixed $WKLY_ID
-     */
+  
     public function setWKLYID($WKLY_ID)
     {
         $this->WKLY_ID = $WKLY_ID;
     }
- 
-    /**
-     * @return mixed
-     */
+  
     public function getREPORTDATE()
     {
         return $this->REPORT_DATE;
     }
- 
-    /**
-     * @param mixed $REPORT_DATE
-     */
+  
     public function setREPORTDATE($REPORT_DATE)
     {
         $this->REPORT_DATE = $REPORT_DATE;
     }
- 
-    /**
-     * @return mixed
-     */
+  
     public function getTOTALHRS()
     {
         return $this->TOTAL_HOURS;
     }
- 
-    /**
-     * @param mixed $TOTAL_HOURS
-     */
+  
     public function setTOTALHRS($TOTAL_HOURS)
     {
         $this->TOTAL_HOURS = $TOTAL_HOURS;
     }
- 
-    /**
-     * @return mixed
-     */
+  
     public function getEMPID()
     {
         return $this->EMP_ID;
     }
- 
-    /**
-     * @param mixed $EMP_ID
-     */
+  
     public function setEMPID($EMP_ID)
     {
         $this->EMP_ID = $EMP_ID;
     }
- 
-    /**
-     * @return mixed
-     */
+  
     public function getEMPNAME()
     {
         return $this->EMPLOYEE_NAME;
     }
- 
-    /**
-     * @param mixed $EMPLOYEE_NAME
-     */
+  
     public function setEMPNAME($EMPLOYEE_NAME)
     {
         $this->EMPLOYEE_NAME = $EMPLOYEE_NAME;
