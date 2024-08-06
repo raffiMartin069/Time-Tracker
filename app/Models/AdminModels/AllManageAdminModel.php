2967aaf68eb748c206a8b21a4b524dfe78cf04c7
airielle <122017843+elruin@users.noreply.github.com>
2024-08-07 02:06:34 +0800

    Modified names

--- app/Models/AdminModels/AllManageAdminModel.php
+++ app/Models/AdminModels/AllManageAdminModel.php
@@ -10,33 +10,22 @@ final class AllManageAdminModel
         $this->EMPLOYEE = $data['EMPLOYEE'];  
       }
   
-    /**
-     * @return mixed
-     */
     public function getEMPID()
     {
         return $this->ID;
     }
 
-    /**
-     * @param mixed $ID
-     */
     public function setEMPID($ID)
     {
         $this->ID = $ID;
     } 
 
-    /**
-     * @return mixed
-     */
     public function getEMPNAME()
     {
         return $this->EMPLOYEE;
     }
 
-    /**
-     * @param mixed $EMPLOYEE
-     */
+
     public function setEMPNAME($EMPLOYEE)
     {
         $this->EMPLOYEE = $EMPLOYEE;
