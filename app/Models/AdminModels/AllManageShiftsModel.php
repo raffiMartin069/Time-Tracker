7776cc1753789145533974ba5fd6d4b9b042fd42
airielle <122017843+elruin@users.noreply.github.com>
2024-08-07 02:07:32 +0800

    Modified names

--- app/Models/AdminModels/AllManageShiftsModel.php
+++ app/Models/AdminModels/AllManageShiftsModel.php
@@ -9,34 +9,22 @@ final class AllManageShiftsModel
         $this->SHIFTING_ID = $data['SHIFTING_ID']; 
         $this->SHIFTING_NAME = $data['SHIFTING_NAME'];  
       }
-  
-    /**
-     * @return mixed
-     */
+   
     public function getSHIFTID()
     {
         return $this->SHIFTING_ID;
     }
-
-    /**
-     * @param mixed $SHIFTING_ID
-     */
+ 
     public function setSHIFTID($SHIFTING_ID)
     {
         $this->SHIFTING_ID = $SHIFTING_ID;
     } 
 
-    /**
-     * @return mixed
-     */
     public function getSHIFTNAME()
     {
         return $this->SHIFTING_NAME;
     }
-
-    /**
-     * @param mixed $SHIFTING_NAME
-     */
+ 
     public function setSHIFTNAME($SHIFTING_NAME)
     {
         $this->SHIFTING_NAME = $SHIFTING_NAME;
