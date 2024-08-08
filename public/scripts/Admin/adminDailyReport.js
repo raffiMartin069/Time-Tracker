9067ae456c863e40d2e59571aedf686e26e83dc8
airielle <122017843+elruin@users.noreply.github.com>
2024-08-08 14:16:19 +0800

    Added update report functionality

--- public/scripts/Admin/adminDailyReport.js
+++ public/scripts/Admin/adminDailyReport.js
@@ -53,10 +53,290 @@ $(".view-breaks-btn").click(function () {
         }
         content += "</tbody></table>";
         $accordionBody.html(content);
-      },
+      }, 
       error: function (xhr, status, error) {
-        $accordionBody.html("Error fetching break data");
+        var errorMessage = "An error has occured while fetching break data. Please try again later.";
+        if (xhr.responseJSON && xhr.responseJSON.error) {
+          errorMessage = xhr.responseJSON.error;
+        }
+  
+        $accordionBody.html(errorMessage);
       },
     });
   }
+});
+
+const editButtons = document.querySelectorAll(".edit-report-btn");
+editButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const dailyId = this.getAttribute("data-daily-id");
+    const empId = this.getAttribute("data-emp-id");
+    const empName = this.getAttribute("data-name");
+    const reportDate = this.getAttribute("data-date");
+    const empClockin = this.getAttribute("data-clock-in");
+    const empLunchin = this.getAttribute("data-lunch-in");
+    const empLunchout = this.getAttribute("data-lunch-out");
+    const empClockout = this.getAttribute("data-clock-out");
+
+    document.getElementById("loadingMessage").style.display = "block";
+    // document.getElementById("editReportForm").style.display = "none";
+
+    document.getElementById("modal-employee-name").value = empName;
+    document.getElementById("modal-report-date").value = reportDate;
+    document.getElementById("modal-clock-in").value = empClockin;
+    document.getElementById("modal-lunch-in").value = empLunchin;
+    document.getElementById("modal-lunch-out").value = empLunchout;
+    document.getElementById("modal-clock-out").value = empClockout;
+
+    let originalBreakData = [];
+    // Flag to track success messages of each updated inputs and only display one successful message
+    let successMsg = false;  
+
+    $.ajax({
+      url: "Admin/BreakStamps",
+      method: "GET",
+      data: {
+        daily_id: dailyId,
+      },
+      dataType: "json",
+      success: function (data) {
+        var breakInputs = "";
+        data.reverse().forEach(function (report, index) {
+          breakInputs += `<div class="mb-2" style="text-align: left;">
+                 Break ${index + 1} Start:
+                 <input type="text" class="mt-1 form-control break-start" value="${
+                   report.BREAK_IN
+                 }" data-record-id="${report.RECORD_ID}">
+             </div>
+             <div class="mb-2" style="text-align: left;">
+                 Break ${index + 1} End:
+                 <input type="text" class="mt-1 form-control break-end" value="${
+                   report.BREAK_OUT
+                 }" data-record-id="${report.RECORD_ID}">
+             </div>`;
+        });
+        document.getElementById("breakInputs").innerHTML = breakInputs;
+        originalBreakData = data;
+
+        // Hide loading message and show the form
+        document.getElementById("loadingMessage").style.display = "none";
+        document.getElementById("editReportForm").style.display = "block";
+      },
+      error: function (xhr, status, error) {
+        var errorMessage = "Something went wrong. Please try again later.";
+        if (xhr.responseJSON && xhr.responseJSON.error) {
+          errorMessage = xhr.responseJSON.error;
+        }
+  
+        Swal.fire({
+          title: "Error",
+          text: errorMessage,
+          icon: "error",
+        });
+  
+        $(".swal2-confirm").click(function () {
+          location.reload();
+        });
+      },
+    });
+
+    document
+      .getElementById("updateReport")
+      .addEventListener("click", function () {
+        const modalClockIn = document.getElementById("modal-clock-in").value;
+        const modalClockOut = document.getElementById("modal-clock-out").value;
+        const modalLunchIn = document.getElementById("modal-lunch-in").value;
+        const modalLunchOut = document.getElementById("modal-lunch-out").value;
+
+        const timeFormat = /^(0?[1-9]|1[0-2]):[0-5][0-9]:[0-5][0-9] (AM|PM)$/i;
+
+        if (empClockin !== modalClockIn) {
+          $.ajax({
+            url: "Admin/UpdateClockInReport",
+            method: "POST",
+            data: {
+              daily_id: dailyId,
+              report_date: reportDate,
+              clock_in: modalClockIn,
+            },
+            success: function (data) {
+              if (!successMsg) {
+                $("#editReportModal").modal("hide");
+                if (timeFormat.test(modalClockIn)) {
+                  Swal.fire({
+                    title: "Success",
+                    text: "Report has been updated successfully!",
+                    icon: "success",
+                  }).then(() => location.reload());
+                  successMsg = true;
+                } else {
+                  Swal.fire({
+                    title: "Error",
+                    html: "Failed to update report!<br>Invalid time format. Please use HH:MM:SS AM/PM.",
+                    icon: "error",
+                  }).then(() => location.reload());
+                }
+              }
+            },
+            error: function (xhr, status, error) {
+              if (!successMsg) {
+                Swal.fire({
+                  title: "Error",
+                  html: "Failed to update report!<br>Please make sure updated entries are within the correct range: not earlier, equal, or later than other time stamps.",
+                  icon: "error",
+                });
+                successMsg = true;
+              }
+            },
+          });
+        }
+
+        if (empClockout !== modalClockOut) {
+          $.ajax({
+            url: "Admin/UpdateClockOutReport",
+            method: "POST",
+            data: {
+              daily_id: dailyId,
+              report_date: reportDate,
+              clock_out: modalClockOut,
+            },
+            success: function (data) {
+              if (!successMsg) {
+                $("#editReportModal").modal("hide");
+                if (timeFormat.test(modalClockOut)) {
+                  Swal.fire({
+                    title: "Success",
+                    text: "Report has been updated successfully!",
+                    icon: "success",
+                  }).then(() => location.reload());
+                  successMsg = true;
+                } else {
+                  Swal.fire({
+                    title: "Error",
+                    html: "Failed to update report!<br>Invalid time format. Please use HH:MM:SS AM/PM.",
+                    icon: "error",
+                  }).then(() => location.reload());
+                }
+              }
+            },
+            error: function (xhr, status, error) {
+              if (!successMsg) {
+                Swal.fire({
+                  title: "Error",
+                  html: "Failed to update report!<br>Please make sure updated entries are within the correct range: not earlier, equal, or later than other time stamps.",
+                  icon: "error",
+                });
+                successMsg = true;
+              }
+            },
+          });
+        }
+
+        if (empLunchin !== modalLunchIn || empLunchout !== modalLunchOut) {
+          $.ajax({
+            url: "Admin/UpdateLunchReport",
+            method: "POST",
+            data: {
+              daily_id: dailyId,
+              emp_id: empId,
+              report_date: reportDate,
+              lunch_in: modalLunchIn,
+              lunch_out: modalLunchOut,
+            },
+            success: function (data) {
+              if (!successMsg) {
+                $("#editReportModal").modal("hide");
+                if (
+                  timeFormat.test(modalLunchIn) &&
+                  timeFormat.test(modalLunchOut)
+                ) {
+                  Swal.fire({
+                    title: "Success",
+                    text: "Report has been updated successfully!",
+                    icon: "success",
+                  }).then(() => location.reload());
+                  successMsg = true;
+                } else {
+                  Swal.fire({
+                    title: "Error",
+                    html: "Failed to update report!<br>Invalid time format. Please use HH:MM:SS AM/PM.",
+                    icon: "error",
+                  }).then(() => location.reload());
+                }
+              }
+            },
+            error: function (xhr, status, error) {
+              if (!successMsg) {
+                Swal.fire({
+                  title: "Error",
+                  html: "Failed to update report!<br>Please make sure updated entries are within the correct range: not earlier, equal, or later than other time stamps.",
+                  icon: "error",
+                });
+                successMsg = true;
+              }
+            },
+          });
+        }
+
+        const breakInputs = document.querySelectorAll("#breakInputs input");
+        for (let i = 0; i < breakInputs.length; i += 2) {
+          const breakIn = breakInputs[i].value;
+          const breakOut = breakInputs[i + 1].value;
+          const recordId = breakInputs[i].getAttribute("data-record-id");
+
+          if (originalBreakData[i / 2]) {
+            if (
+              breakIn !== originalBreakData[i / 2].BREAK_IN ||
+              breakOut !== originalBreakData[i / 2].BREAK_OUT
+            ) {
+              $.ajax({
+                url: "Admin/UpdateBreakReport",
+                method: "POST",
+                data: {
+                  daily_id: dailyId,
+                  record_id: recordId,
+                  emp_id: empId,
+                  report_date: reportDate,
+                  break_in: breakIn,
+                  break_out: breakOut,
+                },
+                success: function (data) {
+                  if (!successMsg) {
+                    $("#editReportModal").modal("hide");
+                    if (timeFormat.test(breakIn) && timeFormat.test(breakOut)) {
+                      Swal.fire({
+                        title: "Success",
+                        text: "Report has been updated successfully!",
+                        icon: "success",
+                      }).then(() => location.reload());
+                      successMsg = true;
+                    } else {
+                      Swal.fire({
+                        title: "Error",
+                        html: "Failed to update report!<br>Invalid time format. Please use HH:MM:SS AM/PM.",
+                        icon: "error",
+                      }).then(() => location.reload());
+                    }
+                  }
+                },
+                error: function (xhr, status, error) {
+                  if (!successMsg) {
+                    Swal.fire({
+                      title: "Error",
+                      html: "Failed to update report!<br>Please make sure updated entries are within the correct range: not earlier, equal, or later than other time stamps.",
+                      icon: "error",
+                    });
+                    successMsg = true;
+                  }
+                },
+              });
+            }
+          }
+        }
+
+        $("#closeReport").click(function () {
+          $("#editReportModal").modal("hide");
+        });
+      });
+  });
 });
